023c64e7c98fea4a3dad2f66f2949352a1bf7094
orahbaloyi1 <orahbaloyi1@gmail.com>
2023-07-22 23:18:15 +0200

    connection pool increased

--- db.py
+++ db.py
@@ -1,14 +1,13 @@
 import os
 from psycopg2 import pool
 
-conn_pool = pool.SimpleConnectionPool(
-    1, 1000000,
-    database="job listing Api",
-    host="localhost",
-    port="5432",
-    user="postgres",
-    password="5432"
-    
+conn_pool = pool.SimpleConnectionPool (
+    1, 10000000000,
+    database=os.getenv('DB_NAME'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_POST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASS')
 )
 class db:
     def __init__(self, table):
@@ -20,19 +19,35 @@ class db:
         cursor = conn.cursor()
         
         sql = f"INSERT INTO {self.table} ({columns}) VALUES ({values})"
-       
         cursor.execute(sql)
-
         conn.commit()
-        
         cursor.close()
         conn.close()
+        
+    ##def insert(self, columns, values):
+     ##   with self.pool.getconn() as conn:
+      ##      with conn.cursor() as cursor:
+            ##    sql = f"INSERT INTO {self.table} ({columns}) VALUES ({values})"
+              ##  cursor.execute(sql)
+          ##  conn.commit()
 
 
-    def select(self, condition=""):
+    ##def select(self, condition=""):
+     ##   with self.pool.getconn() as conn:
+       ##     with conn.cursor() as cursor:
+           ##     sql = f"SELECT * FROM {self.table} {condition};"
+             ##   cursor.execute(sql)
+              ##  rows = cursor.fetchall()
+               #3 return rows 
+       
+       
+
+    def select(self,condition=''):
         conn = self.pool.getconn()
         cursor = conn.cursor()
 
+       # sql = f"SELECT {columns} FROM {self.table} {str(joins_stat or '')} {str(condition or '')};"
+       
         sql = f"SELECT * FROM {self.table} {condition};"
 
         cursor.execute(sql)
@@ -42,4 +57,5 @@ class db:
         conn.close()
 
         return rows  
-    
+       
+      
